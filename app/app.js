c69c6dfba6e90c03d15e60cb0361873a02249b73
Jared Fuller <jaredshane@gmail.com>
2017-02-07 13:45:34 -0600

    linked app and set ng-view and added script tag for controller

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@ console.log('hey')
 const app = angular.module('theClubhouse', ['ngRoute'])
 
 
+
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
 
   $routeProvider
     .when('/', {
-      controller: ,
-      templateUrl: 
+      controller: 'MainCtrl',
+      templateUrl: 'partials/main.html'
     })
 })
