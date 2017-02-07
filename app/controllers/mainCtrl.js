fb267bc13abd06215c0cdc251b73fff4dfe04665
Jared Fuller <jaredshane@gmail.com>
2017-02-07 14:12:14 -0600

    added ng-repeat and getting title/link/description on the dom

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -3,20 +3,23 @@ console.log("mainCtrl");
 app.controller('MainCtrl', ['$http', '$scope', function ($http, $scope) {
 
   $http
-    .get('http://mlb.mlb.com/partnerxml/gen/news/rss/stl.xml')
+    .get('http://mlb.mlb.com/partnerxml/gen/news/rss/atl.xml')
     .then(function(data) {
       console.log("data", data);
 
       let text, parser, xmlDoc;
 
       text = data.data
-
       parser = new DOMParser();
       xmlDoc = parser.parseFromString(text, 'text/xml');
       console.log("xmlDoc", xmlDoc);
 
+      $scope.lists = xmlDoc.getElementsByTagName("item")
+
       let whatever = xmlDoc.getElementsByTagName("item")
-      console.log("whatever[1]", whatever[1]);
+      console.log("whatever", whatever);
+
+      let blah = whatever[1].getElementsByTagName('')
 
       //gets the title of the first item and sets it to the scope
       let title = whatever[1].getElementsByTagName('title')[0].innerHTML
