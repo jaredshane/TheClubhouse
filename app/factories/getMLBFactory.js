f294e09b6f0cc46f7bb2b13b671c767b579634e2
Jared Fuller <jaredshane@gmail.com>
2017-02-10 15:45:30 -0600

    commented out a function in the factory and renamed another function

--- app/factories/getMLBFactory.js
+++ app/factories/getMLBFactory.js
@@ -1,32 +1,33 @@
 app.factory('getMLBFactory', function ($http, $q) {
   let parseData = {};
-  let parseDataTest= {};
   var newArray = []
 
   return {
-    setData : (newArray) => {
-      return $q.all(newArray)
-        .then(function(data) {
-          console.log("data", data);
-          for (var i = 0; i < data.length; i++) {
-            let text = data[i].data
-            let parser = new DOMParser();
-            let xmlDoc = parser.parseFromString(text, 'text/xml');
-            console.log("xmlDoc", xmlDoc)
-            parseData[i]= xmlDoc.getElementsByTagName("item")
-            console.log("parseData", parseData);
-          }
-
-          return parseData
-        })
-    },//setData
+    // setData : (newArray) => {
+    //   return $q.all(newArray)
+    //     .then(function(data) {
+    //       console.log("data", data);
+    //       for (var i = 0; i < data.length; i++) {
+    //         let text = data[i].data
+    //         let parser = new DOMParser();
+    //         let xmlDoc = parser.parseFromString(text, 'text/xml');
+    //         console.log("xmlDoc", xmlDoc)
+    //         parseData[i]= xmlDoc.getElementsByTagName("item")
+    //         console.log("parseData", parseData);
+    //       }
+    //
+    //       return parseData
+    //     })
+    // },//setData
     getData : () => {
       return parseData;
     },//getData
-    setDataTest : (arrayofTeams) => {
+    setData : (arrayofTeams) => {
       console.log("arrayofTeams", arrayofTeams);
       for (var i = 0; i < arrayofTeams.length; i++) {
-        newArray.push($http.get(arrayofTeams[i]))
+        // newArray.push(arrayofTeams[i].teamname)
+        newArray.push($http.get(arrayofTeams[i].mlb))
+        console.log("arrayofTeams[i].teamname", newArray);
       }
       return $q.all(newArray)
         .then(function(data) {
