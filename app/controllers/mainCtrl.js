a96c73888b8f6ca249130e36bd8754c0e316af5b
Jared Fuller <jaredshane@gmail.com>
2017-02-14 11:08:01 -0600

    reworked getting data from factory since I changed how I'm getting data from the factory

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,17 +1,45 @@
 
 app.controller('MainCtrl', function ($scope, getMLBFactory, getTeamFactory) {
 
-    $scope.test = () => {
-      // console.log("getMLBFactory.getData()", getMLBFactory.getData()[0][2].getElementsByTagName('link'));
-      $scope.lists = getMLBFactory.getData()
-      // .then(function (lists) {
-      //   console.log(lists)
-      //   $scope.lists = lists
-      console.log("$scope.lists", $scope.lists);
 
-  }
 
-  $scope.test()
+    let dataArray = []
+    let testArray = []
+    let teamNameArray = []
+
+    let datafromFactory = getMLBFactory.getData()
+    dataArray.push(datafromFactory)
+    console.log("datafromFactory", datafromFactory);
+    console.log("dataArray[0]", dataArray[0]);
+
+
+    for (var j in dataArray[0]) {
+      console.log("j", j);
+      console.log("dataArray[0][j]", dataArray[0][j].rss.channel.item);
+      testArray.push(dataArray[0][j].rss.channel)
+      console.log("testArray", testArray);
+      $scope.lists = testArray
+    }
+
+    // 
+    // $scope.assignTeamName = function(teamCode) {
+    //   if (teamNameArray.includes(teamCode)) {
+    //     return
+    //   }
+    //   var team;
+    //   if (teamCode === "atl") {
+    //     team = "Atlanta Braves";
+    //     teamNameArray.push(teamCode)
+    //     console.log("teamNameArray", teamNameArray);
+    //   }
+    //   else if (teamCode === "sln") {
+    //     team = "St. Louis Cardinals";
+    //     console.log("teamNameArray", teamNameArray);
+    //     teamNameArray.push(teamCode)
+    //   }
+    //   console.log("team", team);
+    //   return team;
+    // }
 
 
 
