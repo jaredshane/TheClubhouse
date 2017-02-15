e71a6e00619f4a0169c4228ac75afb9f7785e6c4
Jared Fuller <jaredshane@gmail.com>
2017-02-15 11:25:46 -0600

    added firebase initialization

--- app/app.js
+++ app/app.js
@@ -3,6 +3,15 @@ const app = angular.module('theClubhouse', ['ngRoute'])
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
 
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyCY8KnwE_caToFQDL2qEDH7JeB42SRCzGQ",
+    authDomain: "the-clubhouse-1f0b3.firebaseapp.com",
+    databaseURL: "https://the-clubhouse-1f0b3.firebaseio.com",
+    storageBucket: "the-clubhouse-1f0b3.appspot.com",
+    messagingSenderId: "598349649037"
+  });
+
   $routeProvider
     .when('/', {
       controller: 'MainCtrl',
