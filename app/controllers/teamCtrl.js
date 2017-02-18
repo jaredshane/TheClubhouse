fb823bb0ce3d58118356ff6fff9b3476dfc64b0e
Jared Fuller <jaredshane@gmail.com>
2017-02-17 19:25:33 -0600

    added Cubs easter egg

--- app/controllers/teamCtrl.js
+++ app/controllers/teamCtrl.js
@@ -53,6 +53,9 @@ $scope.favoriteSave = () => {
         return $scope.arrayofTeams.splice(i, 1)
       }
     }
+      if (team === "Chicago Cubs") {
+        window.location='https://www.google.com/search?q=how+many+world+series+have+the+cardinals+own&oq=how+many+world+series+have+the+cardinals+own&aqs=chrome..69i57j0l2j5.5631j0j7&sourceid=chrome&ie=UTF-8#q=How+many+more+World+Series%27+have+the+Cardinals+won+than+the+Cubs'
+      }
       $scope.arrayofTeams.push(teamListObj[key][team])
       console.log("teamListObj[key][team]", team);
 
