7d99272f2d12e0691013903b140a98eea9e381eb
Jared Fuller <jaredshane@gmail.com>
2017-02-21 12:00:01 -0600

    changed name of database directory from mockdb to teamdatabase

--- app/factories/getTeamFactory.js
+++ app/factories/getTeamFactory.js
@@ -2,7 +2,7 @@ app.factory('getTeamFactory', function ($http) {
   return{
     getTeamList: () => {
       return $http
-        .get('mockdb/teamdb.json')
+        .get('teamdatabase/teamdb.json')
         .then(function (teams) {
           // console.log('getTeamList', teams.data.teams)
           return teams.data.teams
