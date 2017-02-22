0bcd86330419bb2fdfc66aa81ec1d751fdf04403
Jared Fuller <jaredshane@gmail.com>
2017-02-22 12:42:44 -0600

    added factory to get data from a team's selected subreddit and return that data

---
+++ app/factories/getRedditFactory.js
@@ -0,0 +1,32 @@
+app.factory('getRedditFactory', function ($q, $http) {
+
+  let parseData = {};
+  let newArray = []
+  var httpArray = []
+
+
+  return {
+    setData : (arrayofTeams) => {
+      for (var i = 0; i < arrayofTeams.length; i++) {
+        httpArray.push($http.get(arrayofTeams[i].reddit))
+      }
+      return $q.all(httpArray)
+        .then(function (data) {
+          console.log("reddit data", data);
+
+          for (var i = 0; i < data.length; i++) {
+            parseData[i] = data[i]
+          }
+
+          console.log("parseData", parseData);
+          return parseData;
+
+        })
+
+
+    },//setData
+    getData : () => {
+      return parseData;
+    },//getData
+  }
+})
