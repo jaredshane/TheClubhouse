3d8140c0ab6dfd03dadc7c026f7c72e4b81eeacc
Jared Fuller <jaredshane@gmail.com>
2017-02-22 12:40:51 -0600

    deleted a setdata function that was no longer being used

--- app/factories/getMLBTRFactory.js
+++ app/factories/getMLBTRFactory.js
@@ -7,33 +7,7 @@ app.factory('getMLBTRFactory', function ($http, $q) {
     getData : () => {
       return parseData;
     },//getData
-    // setData : (arrayofTeams) => {
-    //   console.log("arrayofTeams", arrayofTeams);
-    //   for (var i = 0; i < arrayofTeams.length; i++) {
-    //     var newObj = {}
-    //     // newArray.push(arrayofTeams[i].teamname)
-    //     newObj.teams = arrayofTeams[i].teamname
-    //     newArray.push(newObj)
-    //     // console.log("newObj", newObj);
-    //
-    //     httpArray.push($http.get(arrayofTeams[i].mlb))
-    //
-    //   }
-    //   return $q.all(httpArray)
-    //     .then(function(data) {
-    //       // console.log("data", data);
-    //       for (var i = 0; i < data.length; i++) {
-    //         let text = data[i].data
-    //         // console.log("text", text);
-    //         let parser = new DOMParser();
-    //         let xmlDoc = parser.parseFromString(text, 'text/xml');
-    //         // console.log("xmlDoc", xmlDoc)
-    //         parseData[i]= xmlDoc.getElementsByTagName("item")
-    //         // console.log("parseData", parseData[i]);
-    //       }
-    //
-    //     })
-    // }//setData
+
     setData : (arrayofTeams) => {
       for (var i = 0; i < arrayofTeams.length; i++) {
         httpArray.push($http.get(arrayofTeams[i].mlbtr))
