f63d6b95dbdd5cf184552f313feff57fc39d2520
Jared Fuller <jaredshane@gmail.com>
2017-02-27 14:53:34 -0600

    deleted some unused code

--- app/controllers/newsCtrl.js
+++ app/controllers/newsCtrl.js
@@ -56,60 +56,6 @@ app.controller('NewsCtrl', function ($scope, getMLBFactory, getTeamFactory, getM
     }
 
 
-    //
-    // $scope.assignTeamName = function(teamCode) {
-    //   if (teamNameArray.includes(teamCode)) {
-    //     return
-    //   }
-    //   var team;
-    //   if (teamCode === "atl") {
-    //     team = "Atlanta Braves";
-    //     teamNameArray.push(teamCode)
-    //     console.log("teamNameArray", teamNameArray);
-    //   }
-    //   else if (teamCode === "sln") {
-    //     team = "St. Louis Cardinals";
-    //     console.log("teamNameArray", teamNameArray);
-    //     teamNameArray.push(teamCode)
-    //   }
-    //   console.log("team", team);
-    //   return team;
-    // }
-
-
-
-
-
-
-
-
-
-
-      //
-      // let whatever = xmlDoc.getElementsByTagName("item")
-      // console.log("whatever", whatever);
-      //
-      // let blah = whatever[1].getElementsByTagName('')
-
-      //gets the title of the first item and sets it to the scope
-      // let title = whatever[1].getElementsByTagName('title')[0].innerHTML
-      // console.log("title", title)
-      // $scope.mlbtitle =  title;
-      //
-      // let description = whatever[1].getElementsByTagName('description')[0].innerHTML
-      // console.log("description", description);
-      // $scope.mlbdescription = description
-      //
-      // let link = whatever[1].getElementsByTagName('link')[0].innerHTML
-      // console.log("link", link);
-      // $scope.mlblink = link
-
-  // var xml = "http://mlb.mlb.com/partnerxml/gen/news/rss/stl.xml",
-  //   xmlDoc = $.parseXML( xml ),
-  //   $xml = $( xmlDoc );
-  //   console.log(xmlDoc)
-
-
-    // })
+  
 
 })
