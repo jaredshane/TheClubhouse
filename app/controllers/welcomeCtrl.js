f0c01a23b7fc664c7de516ebe0afc37d8bf960dd
Jared Fuller <jaredshane@gmail.com>
2017-02-27 12:44:46 -0600

    added a funciton to get the favorite teams on the welcome page so can bypass the team selection page

--- app/controllers/welcomeCtrl.js
+++ app/controllers/welcomeCtrl.js
@@ -16,26 +16,43 @@ app.controller('WelcomeCtrl', function (getMLBFactory, $scope, $location, getFav
   // }
   //
   //
-  // $scope.getFavorites = () => {
-  //   getFavoritesFactory.getURL()
-  //     .then((data) => {
-  //       console.log("data", data.data)
-  //       for (var i = 0; i < data.data.length; i++) {
-  //         console.log("data.data[i]", data.data[i]);
-  //         $scope.arrayofTeams.push(data.data[i]);
-  //
-  //       }
-  //       console.log("$scope.arrayofTeams[0]", $scope.arrayofTeams);
-  //
-  //       // for (var i = 0; i < data.data.length; i++) {
-  //       //   data.data[i]
-  //       // }
-  //     })
-  //
-  // }
-  //
-  // $scope.getFavorites()
+  $scope.getFavorites = () => {
+    getFavoritesFactory.getURL()
+      .then((data) => {
+        console.log("data", data.data)
+        for (var i = 0; i < data.data.length; i++) {
+          console.log("data.data[i]", data.data[i]);
+          $scope.arrayofTeams.push(data.data[i]);
 
+        }
+        console.log("$scope.arrayofTeams[0]", $scope.arrayofTeams);
+
+        // for (var i = 0; i < data.data.length; i++) {
+        //   data.data[i]
+        // }
+      })
+
+  }
+
+  $scope.getFavorites()
+
+
+  //sets the data to parse upon clicking the 'get team news' button
+  $scope.loopTeams = () => {
+    getMLBTRFactory.setData($scope.arrayofTeams)
+      .then (function () {
+        getMLBFactory.setData($scope.arrayofTeams)
+        .then(function (data) {
+          getRedditFactory.setData($scope.arrayofTeams)
+            .then(function () {
+              $location.url("/news")
+            }) //then after Reddit Factory
+        }) // then after MLB factory
+
+      }) //then after MLBTR factory
+
+
+  }
 
 
 
